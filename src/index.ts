e2a94b7a7bf2a87448d1f7b8bc8bcd85aab48696
Your Name <you@example.com>
2023-07-31 10:43:57 +0545

    update

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ app.get("/", (req:Request, res:Response) => {
   res.send("Hello, TypeScript with Express!");
 });
 
+app.get("/bookstore",(req,res)=>{
+  res.send("this is the book store and here you can get many type of the books aof the diffrent genre");
+})
+
 // const PORT = 3000;
 app.listen(PORTt, () => {
   console.log(`Server running on port ${PORTt}`);
